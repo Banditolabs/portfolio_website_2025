92c6490495d28846a32cdd8dc6a3ad9cd08f1e3b
Joshua Goss <joshuagoss@MacBookPro.hsd1.or.comcast.net>
2024-12-21 17:17:42 -0800

    Adds blog

--- app/blog.tsx
+++ app/blog.tsx
@@ -1,7 +1,8 @@
-import { Text, View } from "react-native";
+import { View, Text} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context"
 
-export default function Index() {
+
+export default function Blog() {
   return (
     <SafeAreaView
       style={{
