bb97f7dbceaeb85e65365919d18317a783366dc5
Joshua Goss <Joshuagosst@gmail.com>
2024-12-23 20:49:29 -0800

    Adds Project Frame

--- app/index.tsx
+++ app/index.tsx
@@ -7,13 +7,18 @@ import { FlatList, ScrollView } from "react-native-gesture-handler";
 import { Collapsible } from "@/app-example/components/Collapsible";
 import { ThemedView } from "@/components/ThemedView";
 import { ThemedText } from "@/components/ThemedText";
+import { ProjectFrame } from "@/components/ProjectFrame";
 
 export default function Index() {
   const [count, setCount] = useState(0);
 
   const placeholder =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus luctus arcu nec libero accumsan elementum. Proin at porttitor odio, at faucibus libero. Aenean pretium, dolor eu commodo posuere, lorem erat egestas felis, nec gravida felis purus sit amet purus. Nam id scelerisque magna. Nullam et erat sit amet metus tempus dapibus. Quisque non tincidunt justo. Sed pellentesque nunc ac mi faucibus, in dictum urna vulputate. Ut eget nunc a lacus volutpat fermentum. Nulla elit diam, mollis nec aliquet eget, vestibulum quis leo. Sed pulvinar aliquam elit quis feugiat. Maecenas nec imperdiet sem. Phasellus ut enim volutpat sem faucibus blandFusce justo mauris, varius vel lorem quis, sollicitudin auctor sem. Quisque ornare elit sit amet justo tempor ornare. Donec tristique, ipsum id varius sagittis, dolor ante congue nisl, ut varius tellus lectus sit amet libero. Suspendisse potenti. In sed hendrerit leo, quis iaculis elit. Ut varius elementum enim, ac porta nibh aliquet vel. Aenean congue imperdiet neque, quis malesuada augue condimentum sit amet.    Nulla nibh magna, mattis a rutrum eu, gravida sit amet mi. Vivamus tristique ipsum sed urna varius blandit. Phasellus sagittis, felis ac aliquam scelerisque, neque ex laoreet augue, vel ullamcorper diam neque eget eros. Maecenas ante diam, semper id dolor eu, congue molestie massa. Donec ut aliquam erat. Nunc egestas convallis magna, aliquet posuere ex malesuada vel. Vivamus eros nisl, viverra in felis eget, fringilla aliquet augue.    Quisque pellentesque, ante vel aliquam viverra, urna magna tincidunt dolor, ut sagittis velit dolor ac risus. Aliquam at enim cursus, pellentesque tellus vitae, finibus dolor. Fusce finibus velit enim, pretium ultrices odio laoreet et. Aenean sodales tortor enim, quis fringilla lectus pretium sed. Etiam ligula urna, ultrices eget faucibus eget, porttitor eget lacus. Sed quis ante a augue malesuada pulvinar at ac neque. Ut rutrum, risus sit amet congue efficitur, sapien ipsum dapibus mi, eget scelerisque arcu dui placerat arcu. Nulla varius ex sit amet nisi hendrerit, at placerat lectus eleifend. Mauris diam ligula, elementum eget tellus vel, semper venenatis sem. Duis sed erat maximus, laoreet mi nec, eleifend elit.   Vivamus maximus nec ante at ultrices. Vivamus luctus risus volutpat magna consectetur, ut luctus massa pretium. Quisque a nulla neque. Etiam eros lectus, sagittis luctus auctor sit amet, aliquam id urna. Praesent posuere, velit ac ullamcorper efficitur, leo augue placerat eros, sit amet dictum dui mi dignissim lectus. Aenean ac augue laoreet, pulvinar arcu eget, faucibus velit. Vestibulum fermentum id metus at blandit. Maecenas ut orci at massa feugiat mattis. Integer molestie est erat, sed imperdiet orci ultricies faucibus.";
-  // Add static images
+  const projectData = [
+    {
+      project: {},
+    },
+  ];
   const skillData = [
     {
       icon: (
@@ -70,20 +75,16 @@ export default function Index() {
       ),
     },
   ];
-  type Itemprops = { image_path: string; title: string };
-  const Item = ({ item }) => {
+  type Item = { icon: Image };
+  type ItemProps = { item: Item };
+
+  const Item = ({ item }: ItemProps) => {
     return <View style={styles.item}>{item.icon}</View>;
   };
 
   return (
     <ScrollView style={styles.screen}>
-      <SafeAreaView
-      // style={{
-      //   flex: 1,
-      //   justifyContent: "center",
-      //   alignItems: "center",
-      // }}
-      >
+      <SafeAreaView>
         <Stack.Screen
           options={{
             headerTitle: (props) => false,
@@ -129,6 +130,13 @@ export default function Index() {
         </ThemedView>
         <ThemedView style={[styles.centering, styles.work]}>
           <ThemedText>Work</ThemedText>
+          <View>
+            <ProjectFrame
+              text={"Testing"}
+              image_source={"@/assets/images/rails.svg"}
+            />
+          </View>
+          <View></View>
         </ThemedView>
         <ThemedView style={[styles.centering, styles.contact_me]}>
           <ThemedText>Contact Me</ThemedText>
