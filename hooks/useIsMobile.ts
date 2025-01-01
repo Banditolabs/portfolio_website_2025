c3bb2053a6c83a51e3c4fa2b44dfeb60c5cc7953
Banditolabs <Joshuagosst@gmail.com>
2024-12-31 17:19:22 -0800

    fix for IOS screen width

--- hooks/useIsMobile.ts
+++ hooks/useIsMobile.ts
@@ -1,17 +1,39 @@
-import { useWindowDimensions } from "react-native";
+import { Platform, useWindowDimensions } from "react-native";
 import { useState, useEffect } from "react";
 
 export const MOBILE_BREAKPOINT = 768;
 
 export function useIsMobile() {
   const { width } = useWindowDimensions();
-  const [isMobile, setIsMobile] = useState(
-    typeof window !== "undefined" ? window.innerWidth < MOBILE_BREAKPOINT : false
-  );
+  const [isMobile, setIsMobile] = useState( false  );
 
 
   useEffect(() => {
-    setIsMobile(width < 768);
+    const checkMobile = () => {
+      if (Platform.OS === "web") {
+        const viewportWidth = Math.min(
+          window.innerWidth,
+          document.documentElement.clientWidth
+        );
+        const screenWidth = window.screen.width;
+
+        setIsMobile(Math.min(viewportWidth, screenWidth) < MOBILE_BREAKPOINT);
+      } else {
+        setIsMobile(width < MOBILE_BREAKPOINT);
+      }
+    };
+
+    checkMobile();
+
+    if (Platform.OS === "web") {
+      window.addEventListener("resize", checkMobile);
+    }
+
+    return () => {
+      if (Platform.OS === "web") {
+        window.removeEventListener("resize", checkMobile);
+      }
+    };
   }, [width]);
 
   return isMobile;
